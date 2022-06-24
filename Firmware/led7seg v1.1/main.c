30249f1c7cd29e330ed03daee3ba73da1069512f
chien <chiendt950@gmail.com>
2022-06-24 18:25:43 +0700

    updated

--- Firmware/led7seg v1.1/main.c
+++ Firmware/led7seg v1.1/main.c
@@ -1,30 +1,37 @@
 /*
 Nguoi viet : Nguyen Chien 
 Ngay viet  : 22/06/2022
-De bai : Bam nut nhan tang led -su dung ic 7447
+De bai : Bam nut nhan tang led -su dung ic 7447 gia ma BCD
 */
-
 #include <main.h>
 #include <..\lib\delay.h>
-#define BTN P2_0
-
+#define TANG P2_0
+#define GIAM P2_1
 
 unsigned char  i  = 0 ;
-
 
 void main ()
 {
    while (1)
 	 {
-		 if (BTN== 0)
+		 if ( TANG == 0 && GIAM == 1)
 		 {
 			 i++;
 			 if (i==10) {i=0;}
 			 P1 = i;
-			 while (BTN == 0); // chong hien tuong dinh phim
-			 
-		 
+			 while ( TANG == 0); // chong hien tuong dinh phim
 		 }
+		 else if  ( TANG == 1 && GIAM == 0 )
+		 {
+			 
+			 i--;
+			 if (i==0) {i=9;}
+			 P1 = i;
+			 while ( GIAM == 0); // chong hien tuong dinh phim
+			 
+		 }
+		 else {}
+		 
 	 
 	 
 	 }
--- Firmware/led7segv1.0/main.c
+++ Firmware/led7segv1.0/main.c
@@ -5,7 +5,8 @@ De bai : nhan nut tang 1 so tren led 7 thanh
 */
 #include <REGX51.H>
 #include <..\lib\Delay.h>
-#define BTN P2_0
+#define TANG P2_0
+#define GIAM P2_1
 
 unsigned char  i  = 0 ;
 unsigned char  code_7seg[10] = {0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8, 0x80, 0x90};
@@ -13,12 +14,21 @@ unsigned char  code_7seg[10] = {0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8, 
 void main ()
 {	 
    while (1)
-   { if (BTN == 0)
+   { if ( TANG == 0 && GIAM == 1 )
 		     {  
 					  i++;
 					  if (i==10) i=0;
 				    P3 = code_7seg[i];
-					   while (BTN == 0) ; //chong hien tuong dinh phim
+					   while ( TANG == 0) ; //chong hien tuong dinh phim
 				 }
+			else if ( TANG == 1 && GIAM == 0 )
+			   {
+            i--;
+					  if (i==0) i=9;
+				    P3 = code_7seg[i];
+					  while (GIAM == 0) ; //chong hien tuong dinh phim
+            					 
+				 }
+			else {}
    }
 }
